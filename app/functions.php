21b94ec27c87b85376eee1c4795f9f2e63f21f42
Anton Melnikov <antokhamelnikov@yandex.ru>
2019-08-11 18:56:10 +0300

    Functions

--- app/functions.php
+++ app/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// автозагрузка классов
 function autoload($className) {
   if(file_exists(ROOT . DS . 'app' . DS . $className . '.php')) {
     require_once(ROOT . DS . 'app' . DS . $className . '.php');
@@ -7,3 +8,42 @@ function autoload($className) {
 }
 
 spl_autoload_register('autoload');
+//-----------------------------------
+
+// транслитерация
+function translit($stroka) {
+  $converter = array(
+'а' => 'a', 'б' => 'b', 'в' => 'v',
+'г' => 'g', 'д' => 'd', 'е' => 'e',
+'ё' => 'e', 'ж' => 'zh', 'з' => 'z',
+'и' => 'i', 'й' => 'y', 'к' => 'k',
+'л' => 'l', 'м' => 'm', 'н' => 'n',
+'о' => 'o', 'п' => 'p', 'р' => 'r',
+'с' => 's', 'т' => 't', 'у' => 'u',
+'ф' => 'f', 'х' => 'kh', 'ц' => 'ts',
+'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch',
+'ь' => '', 'ы' => 'y', 'ъ' => '',
+'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+'А' => 'A', 'Б' => 'B', 'В' => 'V',
+'Г' => 'G', 'Д' => 'D', 'Е' => 'E',
+'Ё' => 'E', 'Ж' => 'Zh', 'З' => 'Z',
+'И' => 'I', 'Й' => 'Y', 'К' => 'K',
+'Л' => 'L', 'М' => 'M', 'Н' => 'N',
+'О' => 'O', 'П' => 'P', 'Р' => 'R',
+'С' => 'S', 'Т' => 'T', 'У' => 'U',
+'Ф' => 'F', 'Х' => 'Kh', 'Ц' => 'Ts',
+'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Shch',
+'Ь' => '', 'Ы' => 'Y', 'Ъ' => '',
+'Э' => 'E', 'Ю' => 'Yu', 'Я' => 'Ya',
+);
+ return strtr($stroka, $converter);
+}
+
+// создание юзернейма
+function create_username($fname, $lname) {
+  $fname=translit($fname);
+  $lname=translit($lname);
+  $str = substr($fname, 0, 4) . substr($lname, 0, 4);
+  $str = strtolower($str) . substr(strval(time()), -2, 2);
+  return $str;
+}
--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
+
+session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
 
 require_once(ROOT . DS . 'app' . DS . 'functions.php');
 
+
+//-------------------------------------
 echo 'Welcome to MHAdmin!!!</br>';
 echo $_SERVER['PATH_INFO'] . '</br>';
-echo $_SERVER['QUERY_STRING'];
+echo $_SERVER['QUERY_STRING'] . '</br>';
+
+if(isset($_SERVER['PATH_INFO'])) {
+  $url=explode('/', $_SERVER['PATH_INFO']);
+} else {
+  $url=[];
+}
+
+var_dump($url);
+echo "</br>";
+echo translit('Привет Прощай цыпочки цыплёнок');
+echo "</br>";
+echo create_username('Антон', 'Мельников');
