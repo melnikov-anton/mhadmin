3a34d522bdcf5392b4fa30233877ace4ec4a8de5
Anton Melnikov <antokhamelnikov@yandex.ru>
2019-11-07 14:17:18 +0300

    mha-init added

--- mha_init.sh
+++ mha_init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-apt update -y
+#apt update -y
+apt-get update && apt-get install -y apache2 php php-mysql mariadb-server \
+proftpd mc curl python3-pip libapache2-mod-wsgi-py3 && a2enmod rewrite
 
 mysql < config/mysql/mhadmin_db_init.sql
